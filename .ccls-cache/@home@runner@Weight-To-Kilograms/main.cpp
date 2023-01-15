c4bae1b80a30f60373f9217900e7add8496f47af
Marc Jacques <39604945+MarcJacques@users.noreply.github.com>
2023-01-15 21:12:35 +0000

    complete setup

--- .ccls-cache/@home@runner@Weight-To-Kilograms/main.cpp
+++ .ccls-cache/@home@runner@Weight-To-Kilograms/main.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
+using namespace std;
 
-int main() {
+int main() 
+{
 
+  double weight, kilograms;
+  //ask user for weight
+  cout << "What is your weight?" << endl;
+  cin >> weight;
+  
+  //convert lbs to kgs
+  kilograms = 2.2 * weight;
+
+  cout << "Your weight in kilograms is " << kilograms << endl;
+  
 }
